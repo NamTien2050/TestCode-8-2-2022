090345a6b2f0f47d9969d0ba8792a7efd7aa3172
NamTien2050 <nguyenvuhoangthien2050@gmail.com>
2022-02-28 10:02:16 +0700

    up 28-02-2022

--- src/main/java/com/example/testcode/service/sysUser/ISysUserService.java
+++ src/main/java/com/example/testcode/service/sysUser/ISysUserService.java
@@ -1,7 +1,6 @@
 package com.example.testcode.service.sysUser;
 
 import com.example.testcode.model.SysUser;
-//import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface ISysUserService {
     void save(SysUser sysUser);
@@ -9,4 +8,3 @@ public interface ISysUserService {
     Iterable<SysUser> findAll();
 }
 
-//extends UserDetailsService
--- src/main/java/com/example/testcode/service/sysUser/SysUserService.java
+++ src/main/java/com/example/testcode/service/sysUser/SysUserService.java
@@ -1,11 +1,7 @@
 package com.example.testcode.service.sysUser;
 
 import com.example.testcode.model.SysUser;
-//import com.example.testcode.model.account.UserPrinciple;
 import com.example.testcode.repository.ISysUserRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +19,4 @@ public class SysUserService implements ISysUserService {
     public Iterable<SysUser> findAll() {
         return iSysUserRepo.findAll();
     }
-
-//    @Override
-//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-//        SysUser sysUser = iSysUserRepo.findByLoginName(userName);
-//        if (sysUser == null)
-//            throw new UsernameNotFoundException(userName);
-//        return UserPrinciple.build(sysUser);
-//    }
 }
